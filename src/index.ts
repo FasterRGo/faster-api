c755e36f3714701f737a2b14a444261c0a80df96
Gustavo Silva <gustavost1y@gmail.com>
2024-06-16 11:04:40 -0300

    feat: cors added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { router } from './server/routes/';
-// import cors from 'cors'
+import cors from 'cors'
 const app = express()
 
-// app.use(cors({
-//     origin: '*'
-// }))
+app.use(cors({
+    origin: '*'
+}))
 
 app.use(express.json())
 app.use(router)
