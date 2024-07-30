a45ee10f44b038aac7be1dad3e1a25bb38fe6bae
JonesGamer18 <joaotubino.aluno@unipampa.edu.br>
2024-07-30 11:19:02 -0300

    Atualização

--- src/server/routes/userAuth.routes.ts
+++ src/server/routes/userAuth.routes.ts
@@ -6,7 +6,7 @@ const signUp = new UserSignUpController()
 const refresh = new UserRefreshController()
 
 const userAuthRoutes = (router: Router): void => {
-    router.post("/user/auth/sign-in", signIn.execute.bind(UserSignInController));
+    router.get("/user/auth/sign-in", signIn.execute.bind(UserSignInController));
     router.get("/user/auth/refresh", refresh.execute.bind(UserRefreshController));
     router.post("/user/auth/sign-up", signUp.execute.bind(UserSignUpController));
 }
