9e739b98db5a3d40909d9fa960887f3ab71c4a82
JonesGamer18 <joaotubino.aluno@unipampa.edu.br>
2024-07-30 11:25:12 -0300

    Atualização

--- src/controller/user/auth/signin.ts
+++ src/controller/user/auth/signin.ts
@@ -1,49 +1,44 @@
 import { compare } from "bcryptjs";
-import { Request, Response } from 'express';
-import { sign } from 'jsonwebtoken';
+import { Request, Response } from 'express'
+import { sign } from 'jsonwebtoken'
 import { findUserByEmail } from "../../../database/repositories/userRepository";
+import { signInValidator } from "../../../utils/formValidator/userValidator";
 import { env } from "../../../environment";
 
 class UserSignInController {
     async execute(req: Request, res: Response) {
         try {
-            const { email, password } = req.query;
-
-            if (!email || !password) {
-                return res.status(400).json({ message: 'Email and password are required' });
-            }
-
-            const user = await findUserByEmail(email as string);
-            const { AUTH_TOKEN, REFRESH_TOKEN } = env;
+            const { email, password } = await signInValidator.validate(req.body)
+            const user = await findUserByEmail(email)
+            const { AUTH_TOKEN, REFRESH_TOKEN } = env
 
             if (!user) {
-                return res.status(400).json({ message: 'Invalid email or password' });
+                throw new Error('Email ou Senha inválidos')
             }
 
-            const isPasswordValid = await compare(password as string, user.password);
+            const isPasswordValid = await compare(password, user.password)
 
             if (!isPasswordValid) {
-                return res.status(400).json({ message: 'Invalid email or password' });
+                throw new Error('Email ou Senha inválidos')
             }
 
             const id = user.id;
 
             const token = sign({ id }, AUTH_TOKEN as string, {
-                expiresIn: '1h'
-            });
-
+                expiresIn: 3600
+            })
             const refreshT = sign({ id }, REFRESH_TOKEN as string, {
-                expiresIn: '10h'
-            });
+                expiresIn: 36000
+            })
 
-            return res.status(200).json({ user, accessToken: token, refreshToken: refreshT });
+            return res.status(200).json({ user, accessToken: token, refreshToken: refreshT })
 
         } catch (error: any) {
             return res.status(400).json({
                 message: error.message
-            });
+            })
         }
     }
 }
 
-export { UserSignInController };
+export { UserSignInController }
